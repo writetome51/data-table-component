f407f38138e81d8c5d796f190c8bc22695c7b551
Steve Thompson <writetome51@gmail.com>
2019-02-19 00:36:59 -0700

    Added DataSource and PaginatorDataController to items injected into DataTableComponent constructor

--- src/app/data-table.component.ts
+++ src/app/data-table.component.ts
@@ -1,31 +1,35 @@
 import {
-	AfterViewInit, Component, OnInit, ViewChild
+    AfterViewInit, Component, OnInit, ViewChild
 } from '@angular/core';
 import { CollapsibleDetailRowDirective }
-	from './collapsible-detail-row-directive/collapsible-detail-row.directive';
+    from './collapsible-detail-row-directive/collapsible-detail-row.directive';
 import { PaginatorService } from 'src/app/data-table.component';
 
 
 @Component({
-	selector: 'data-table',
-	templateUrl: './data-table.component.html',
-	styleUrls: ['./data-table.component.css']
+    selector: 'data-table',
+    templateUrl: './data-table.component.html',
+    styleUrls: ['./data-table.component.css']
 })
 
 
 export class DataTableComponent implements OnInit, AfterViewInit {
 
 
-	constructor(public paginatorSvc: PaginatorService) {
-	}
+    constructor(
+        public paginatorSvc: PaginatorService,
+        public dataSource: DataSource,
+        public paginatorDataCtlr: PaginatorDataController
+    ) {
+    }
 
 
-	ngOnInit() {
-	}
+    ngOnInit() {
+    }
 
 
-	ngAfterViewInit() {
-	}
+    ngAfterViewInit() {
+    }
 
 
 }
