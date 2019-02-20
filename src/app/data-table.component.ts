c51411ba3b6744f197c2b753d145912ab5f97e1d
Steve Thompson <writetome51@gmail.com>
2019-02-19 17:40:54 -0700

    minor changes to tslint.json

--- src/app/data-table.component.ts
+++ src/app/data-table.component.ts
@@ -1,35 +1,33 @@
 import {
-    AfterViewInit, Component, OnInit, ViewChild
+	AfterViewInit, Component, OnInit, ViewChild
 } from '@angular/core';
 import { CollapsibleDetailRowDirective }
-    from './collapsible-detail-row-directive/collapsible-detail-row.directive';
-import { PaginatorService } from 'src/app/data-table.component';
+	from './collapsible-detail-row-directive/collapsible-detail-row.directive';
+import { PaginatorService } from '@writetome51/paginator-service';
+import { PaginatorDataController } from '@writetome51/paginator-data-controller';
 
 
 @Component({
-    selector: 'data-table',
-    templateUrl: './data-table.component.html',
-    styleUrls: ['./data-table.component.css']
+	selector: 'data-table',
+	templateUrl: './data-table.component.html',
+	styleUrls: ['./data-table.component.css']
 })
-
-
 export class DataTableComponent implements OnInit, AfterViewInit {
 
 
-    constructor(
-        public paginatorSvc: PaginatorService,
-        public dataSource: DataSource,
-        public paginatorDataCtlr: PaginatorDataController
-    ) {
-    }
+	constructor(
+		public paginatorSvc: PaginatorService,
+		public paginatorDataCtlr: PaginatorDataController
+	) {
+	}
 
 
-    ngOnInit() {
-    }
+	ngOnInit() {
+	}
 
 
-    ngAfterViewInit() {
-    }
+	ngAfterViewInit() {
+	}
 
 
 }
